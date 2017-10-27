8ca19442f28bca809f70eb0c6bd738b88a281588
Andy Zhang <zhangandy915@gmail.com>
2017-10-27 13:41:48 -0400

    update 1.2

--- app/src/main/java/com/example/doudo/practicecallingobjectmethods/MainActivity.java
+++ app/src/main/java/com/example/doudo/practicecallingobjectmethods/MainActivity.java
@@ -1,6 +1,5 @@
 package com.example.doudo.practicecallingobjectmethods;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
@@ -10,14 +9,11 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        // Get the Intent that started this activity and extract the string
-        Intent intent = getIntent();
-        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+        TextView textView = new TextView(this);
+        textView.setText("wow");
+        setContentView(textView);
 
         // Capture the layout's TextView and set the string as its text
-        TextView textView = findViewById(R.id.textView);
-        textView.setText(message);
+
     }
 }
